a20d30d14f45c216a68a0248559b1983230973c9
Sakhiur <116958704+Sakhiur2022@users.noreply.github.com>
2026-01-29 22:04:12 +0600

    feat: Implement model training and hyperparameter tuning for house price prediction

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('house_price_bd.csv')
 
-data
-
 data.info()
 
 data.dropna(inplace=True)
@@ -30,3 +28,34 @@ sns.heatmap(train_data.corr(),annot=True,cmap="YlGnBu")
 train_data['price']=np.log(train_data['price']+1)
 
 train_data.hist()
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+reg = LinearRegression()
+X_train,y_train=train_data.drop(['price'],axis=1),train_data['price']
+X_train_s=StandardScaler().fit_transform(X_train)
+reg.fit(X_train,y_train)
+
+test_data=X_test.join(y_test)
+test_data['price']=np.log(test_data['price']+1)
+
+X_test,y_test=test_data.drop(['price'],axis=1),test_data['price']
+X_test_s=StandardScaler().fit_transform(X_test)
+reg.score(X_test,y_test)
+
+from sklearn.ensemble import RandomForestRegressor
+forest=RandomForestRegressor()
+forest.fit(X_train,y_train)
+forest.score(X_test,y_test)
+
+from sklearn.model_selection import GridSearchCV
+forest=RandomForestRegressor()
+param_grid={'n_estimators':[10,20,30,40,50,60,70,80,90,100],
+            'max_depth':[1,2,3,4,5,6,7,8,9,10],
+            'max_features':[1,2,3,4] }
+grid_search=GridSearchCV(forest,param_grid,cv=5,scoring='neg_mean_squared_error',return_train_score=True)
+grid_search.fit(X_test_s,y_test)
+
+best_forest=grid_search.best_estimator_
+best_forest.score(X_test_s,y_test)
